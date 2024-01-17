84a8e5d8cf045ede526d9dfc6d2e4f78b2c412da
ADMIN <ADMIN@PC343>
2024-01-17 10:09:03 +0700

    Add project files.

---
+++ Bai01/Program.cs
@@ -0,0 +1,121 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    class NhanVien
+    {
+        private string HoTen;
+        private int soNgayCong;
+        private int SoNgayLamThem;
+        private string xeploai;
+        private string boPhan;
+
+        public NhanVien ()
+        {
+           
+        }
+        public NhanVien(string HoTen, int soNgayCong, int soNgayLamThem, string xeploai, string boPhan)
+        {
+            this.HoTen = HoTen;
+            this.soNgayCong = soNgayCong;
+            this.SoNgayLamThem = SoNgayLamThem;
+            this.xeploai = xeploai;
+            this.boPhan = boPhan;
+        }
+        public string hoten
+        {
+            get { return HoTen; }
+            set { HoTen = value; }
+        }
+        public int songaycong
+        {
+            get { return soNgayCong; }
+            set { soNgayCong = value; }
+        }
+        public int songaylamthem
+        {
+            get { return SoNgayLamThem; }
+            set { SoNgayLamThem = value; }
+        }
+        public string XepLoai
+        {
+            get { return xeploai; }
+           set { xeploai = value; }
+        }
+        public string bophan
+        {
+            get { return boPhan; }
+            set { boPhan = value; }
+        }
+        public double tinhLuong()
+        {
+            int soNgayLamThemTang = 0;
+            if (bophan.Equals("Truc Tiep:"))
+            {
+                soNgayLamThemTang = songaylamthem * 3;
+
+            }
+            else if (bophan.Equals("GianTiep:"))
+            {
+                soNgayLamThemTang = songaylamthem * 2;
+            }
+            return (soNgayCong + soNgayLamThemTang) * 100000;
+        }
+        public double tinhThuNhap()
+        {
+            double thunhap = tinhLuong();
+                switch (xeploai)
+            {
+                case "A":
+                    thunhap = thunhap * 1.5;
+                    break;
+                case "B":
+                    thunhap = thunhap * 1.2;
+                    break;
+                default:
+                    break;
+            }
+            return thunhap;
+        }
+        public void inThongTin()
+        {
+            Console.WriteLine("Ho ten: " + hoten);
+            Console.WriteLine("Bo phan: " + bophan);
+            Console.WriteLine("Thu Nhap: " + tinhThuNhap());
+        }
+        static void Main(string[] args)
+        {
+            NhanVien nhanvien1 = new NhanVien();
+
+            Console.Write("Nhap Ho Ten Nhan Vien :");
+            nhanvien1.HoTen = Console.ReadLine();
+
+            Console.Write("Nhap So Ngay Cong: ");
+            nhanvien1.songaycong = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap So Ngay Lam them: ");
+            nhanvien1.SoNgayLamThem = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap Xep Loai (A/B/C): ");
+            nhanvien1.xeploai = Console.ReadLine();
+
+            Console.Write("Nhap Bo Phan (Truc tiep hay Gian tiep): ");
+            nhanvien1.boPhan = Console.ReadLine();
+
+            Console.WriteLine("\nIn thong tin nhan vien vua nhap:");
+            nhanvien1.inThongTin();
+
+            NhanVien nhanVien2 = new NhanVien("Le The Nguyen", 18, 3, "A", "Truc Tiep");
+            Console.WriteLine("\nThong Tinh Nhan Vien 2");
+            nhanVien2.inThongTin();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Bai02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai02
+{
+    class ChuNhat
+    {
+        private double rong;
+        private double dai;
+
+        public ChuNhat (double rong, double dai)
+        {
+            this.dai = dai;
+            this.rong = rong;
+        }
+        public double TinhChuvi()
+        {
+            return 2 * (dai + rong);
+        }
+        public double Tinhdientich()
+        {
+            return dai * rong;
+        }
+        public virtual void Xuat()
+        {
+            Console.WriteLine("Chieu dai hinh chu nhat: " + dai);
+            Console.WriteLine("Chieu rong hinh chu nhat: " + rong);
+            Console.WriteLine("Dien tich hinh chu nhat: " + Tinhdientich());
+            Console.WriteLine("Chu Vi hinh chu nhat: " + TinhChuvi());
+        }
+        class Vuong : ChuNhat
+        {
+            public Vuong(double canh) : base(canh, canh) { }
+            public override void Xuat()
+            {
+                Console.WriteLine("Canh Hinh Vuong" + dai);
+                Console.WriteLine("Dien tich: " + Tinhdientich());
+                Console.WriteLine("ChuVi " + TinhChuvi());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap Chieu Dai Hinh Chu Nhat: ");
+            double daiCN = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap Chieu Rong Hinh Chu Nhat: ");
+            double rongCN = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap Canh Hinh Vuong: ");
+            double CanhVuong = double.Parse(Console.ReadLine());
+
+            ChuNhat cn = new ChuNhat(daiCN, rongCN );
+            Vuong vuong = new Vuong(CanhVuong);
+
+            Console.WriteLine("\n Thong Tin Hinh Chu Nhat: ");
+            cn.Xuat();
+
+            Console.WriteLine("\n Thong Tin Hinh Vuong:");
+            vuong.Xuat();
+            Console.ReadLine();
+        }
+    }
+}
